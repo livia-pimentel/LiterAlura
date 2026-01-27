de9c9595662e6ccb35e1045b508f03e7ba6c184a
livia-pimentel <livia_macedo89@yahoo.com.br>
2026-01-27 20:19:51 -0300

    Estrtura inicial da classe principal

--- src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/br/com/alura/LiterAlura/LiterAluraApplication.java
@@ -1,5 +1,6 @@
 package br.com.alura.LiterAlura;
 
+import br.com.alura.LiterAlura.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class LiterAluraApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }
---
+++ src/main/java/br/com/alura/LiterAlura/principal/Principal.java
@@ -0,0 +1,97 @@
+package br.com.alura.LiterAlura.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+    // Atributos
+    private Scanner scanner = new Scanner(System.in);
+
+    // Metodos
+    public void exibeMenu() {
+                var opcao = -1;
+        while (opcao != 0) {
+
+            System.out.println("Escolha o número de sua opção: ");
+
+            var menu = """
+                    1 - Buscar livro pelo título
+                    2 - Listar livros registrados
+                    3 - Listar autores registrados
+                    4 - Listar autores vivos em um determinado ano
+                    5 - Listar livros em um determinado idioma
+                    0 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    buscarLivro();
+                    break;
+                case 2:
+                    listarLivros();
+                    break;
+                case 3:
+                    listarAutores();
+                    break;
+                case 4:
+                    listarAutoresVivos();
+                    break;
+                case 5:
+                    listarIdiomaDoLivro();
+                    break;
+                case 0:
+                    System.out.println("Finalizando a aplicação...");
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+    }
+
+    private void buscarLivro() {
+        System.out.println("------ LIVRO ------");
+        // TITULO DO LIVRO
+        // AUTOR
+        // IDIOMA
+        // NUMERO DE DOWNLOADS
+        System.out.println("-------------------");
+    }
+
+    private void listarLivros() {
+        // DEVE RETORNAR UMA LISTA DE TODOS OS LIVROS QUE FORAM ADICIONADOS AO BANCO
+
+    }
+
+    private void listarAutores() {
+        // DEVE RETORNAR OS DADOS DE TODOS OS AUTORES DO BANCO
+        // NOME DO AUTOR
+        // ANO DE NASCIMENTO
+        // ANOS DE FALECIMENTO
+        // LIVROS DO AUTOR (UMA ARRAY)
+    }
+
+    private void listarAutoresVivos() {
+        // DEVE RETORNAR OS AUTORES VIVOS NO ANO DESEJADO
+        System.out.println("Insira o ano que deseja pesquisar: ");
+        var ano = scanner.nextInt();
+    }
+
+    private void listarIdiomaDoLivro() {
+        System.out.println("Insira o idioma para realizar a busca: ");
+
+        var opcaoIdioma = """
+                          es - espanhol
+                          en - inglês
+                          fr - francês
+                          pt - português
+                          """;
+
+        System.out.println(opcaoIdioma);
+
+        // DEPOIS DE ESCOLHER O IDIOMA DEVE RETORNAR A LISTA DE LIVROS NO IDIOMA DESEJADO
+    }
+
+}
